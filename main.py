e1d9d7c92d001f489b5d337fc0d5333895311dcd
Dmitriy Kulikov <3odop@mail.ru>
2024-08-27 17:31:38 +0500

    Ты вот зачем это читаешь,а?

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from calculations import *
+import psutil
+import time
+from telegram_bot import send_telegram_message  # Импортируем функцию отправки сообщения
+from settings import *
+def check_memory_usage():
+    memory = psutil.virtual_memory()
+    return memory.percent
 
-# Оперативная память 
-RAM_MONITOR = True
-alert_threshold = 70 # порог загруженности RAM
-alert_duration = 60  # Длительность в секундах 
-interval = 1  # Интервал между проверками (в секундах)
+
+def ram():
+    checks_required = alert_duration // interval  # Сколько проверок нужно для 1 минуты
+    high_usage_count = 0
+
+    while True:
+        memory_usage = check_memory_usage()
+        if memory_usage > alert_threshold:
+            high_usage_count += 1
+        else:
+            high_usage_count = 0
+
+        if high_usage_count >= checks_required:
+            message = f"Предупреждение: Загруженность оперативной памяти составляет {memory_usage}%! в течении последних {alert_duration} секунд! "
+            send_telegram_message(message)
+            high_usage_count = 0  # Сброс счетчика после отправки уведомления
+
+        time.sleep(interval)
+if RAM_MONITOR == True:
+    #ram()
+    print(check_memory_usage())
+else: 
+    exit
+
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,25 @@
 import requests
-import configparser
+from configparser import ConfigParser
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-API_TOKEN = config.get('telegram', 'API_bot')
-CHAT_ID = config.get('telegram', 'ID_client')
+def get_config():
+    config = ConfigParser()
+    config.read('config.ini')
+    return {
+        'API_TOKEN': config.get('telegram', 'API_bot'),
+        'CHAT_ID': config.get('telegram', 'ID_client')
+    }
 
 def send_telegram_message(message):
-    url = f"https://api.telegram.org/bot{API_TOKEN}/sendMessage"
+    config = get_config()
+    url = f"https://api.telegram.org/bot{config['API_TOKEN']}/sendMessage"
     payload = {
-        'chat_id': CHAT_ID,
+        'chat_id': config['CHAT_ID'],
         'text': message
     }
-    response = requests.post(url, data=payload)
-    if response.status_code != 200:
-        raise Exception(f"Failed to send message: {response.text}")
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status()  # Проверяем на ошибки HTTP
+        if not response.json().get('ok'):
+            raise Exception(f"Ошибка при отправке сообщения: {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при отправке сообщения: {e}")
